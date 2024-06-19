a8f73c9d724bcc54da5967e4d30080bfe469da46
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-06-19 13:56:36 +0530

    started learning Materia UI

---
+++ src/pages/Create.jsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+
+export function Create() {
+  return (
+    <>
+      <h1>Create</h1>
+      <Typography>Creata new Note</Typography>
+    </>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Typography from "@mui/material/Typography";
+
+export function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Typography></Typography>
+    </>
+  );
+}
