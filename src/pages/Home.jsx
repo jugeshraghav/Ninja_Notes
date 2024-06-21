2f86b64cdb013d29211b5b5609fac4bbfd36b7d0
jugesh raghav <74092377+jugeshraghav@users.noreply.github.com>
2024-06-21 15:08:02 +0530

    edge cases handled

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,27 +16,37 @@ export function Home() {
     <Container>
       <h1>My Notes</h1>
       <Grid container spacing={5}>
-        {notes.map((note) => (
-          <Grid item key={note.id} xs={12} md={6} lg={4}>
-            <Card elevation={2}>
-              <CardHeader
-                action={
-                  <IconButton onClick={() => deleteNoteHandler(note)}>
-                    <DeleteOutline />
-                  </IconButton>
-                }
-                title={note.title}
-                subheader={note.category}
-              />
-              <CardContent>
-                {" "}
-                <Typography variant="body2" color="text.secondary">
-                  {note.text}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
+        {notes.length !== 0 ? (
+          notes.map((note) => (
+            <Grid item key={note.id} xs={12} md={6} lg={4}>
+              <Card elevation={2}>
+                <CardHeader
+                  action={
+                    <IconButton onClick={() => deleteNoteHandler(note)}>
+                      <DeleteOutline />
+                    </IconButton>
+                  }
+                  title={note.title}
+                  subheader={note.category}
+                />
+                <CardContent>
+                  {" "}
+                  <Typography variant="body2" color="text.secondary">
+                    {note.text}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Typography
+            variant="h5"
+            color="primary"
+            sx={{ width: "20vw", margin: "auto", marginTop: "100px" }}
+          >
+            Add your notes!
+          </Typography>
+        )}
       </Grid>
     </Container>
   );
